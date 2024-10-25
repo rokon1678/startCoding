9f0364f3d741d73f371a1dc0b2899014e2021e6c
startCoding <abid302084@gmail.com>
2024-10-25 15:30:51 +0600

    add New js code

--- function/script.js
+++ function/script.js
@@ -1,7 +1,27 @@
 // let's drive into function
 
-function myFunction (FullName, age ) {
-    console.log("Hello I am " + FullName + ' my age is ' + age );
+function myFunction (FullName, age, qualification ) {
+    console.log("Hello I am " + FullName + ' my age is ' + age + ' I am ' + qualification);
 }
 
-myFunction('Rokon', 20 )
+myFunction('Rokon', 20, 'Student')
+
+// crite a subscribed button
+
+function MyFunction () {
+    let checke = document.querySelector("#subscribed").innerHTML= "Subscribed";
+    alert = 'Subscribed';
+   
+    console.log(checke);
+   
+    
+}
+
+let identify = document.querySelector("#subscribed");
+identify.addEventListener ('click', MyFunction);
+
+// auto call function
+
+(function (message){
+    console.log(' I need ' +  message )
+})("your help");
