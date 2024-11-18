c865f933cc23e7f93b55da6f8e6a5c160e899a0a
startCoding <abid302084@gmail.com>
2024-11-18 16:29:54 +0600

    add new file

---
+++ project/cal.js
@@ -0,0 +1,24 @@
+const btnC = document.querySelector("#btn5");
+const btn1 = document.querySelector("#btn1");
+const btn2 = document.querySelector("#btn2");
+const btn3 = document.querySelector("#btn3");
+const btn4 = document.querySelector("#btn4");
+const btn5 = document.querySelector("#btn5");
+const btn6 = document.querySelector("#btn5");
+const btn7 = document.querySelector("#btn5");
+const btn8 = document.querySelector("#btn5");
+const btn10 = document.querySelector("#btn5");
+const btn11 = document.querySelector("#btn5");
+const btn12 = document.querySelector("#btn5");
+const btn13 = document.querySelector("#btn5");
+const btn14 = document.querySelector("#btn5");
+const btn15 = document.querySelector("#btn5");
+const screen = document.querySelector("#calCounter");
+
+btn1.addEventListener("click", function() {
+    screen.innerText = 1;
+    console.log("I am working");
+
+});
+
+
