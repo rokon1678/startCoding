504fe5ba0bde6ad4192cff7d6ff3c72fe51156a5
startCoding <abid302084@gmail.com>
2024-12-10 12:39:16 +0600

    add new folder and file

--- project/rivewe/review.js
+++ project/rivewe/review.js
@@ -20,24 +20,26 @@ let test = 0;
            image.src = "rokon final.png";
            paraText.innerText = "I really like your commpany products. for the first time I thought you do not have much better product in your company but now I realize no that was incorrect Thanks for your better services.";
            nameText.innerText = "croner de";
+        //    console.log("I am working");
                
-                },4000);
+      },4000);
 
-                setTimeout(() => {
+     setTimeout(() => {
    
                     image.src = "piture1.jpg";
                     paraText.innerText = "We’d been using Trusted Shops for around 8 years but it became really expensive. It also didn’t offer much flexibility. We wanted a platform that gave us the features we needed at a fair price.";
                     nameText.innerText = "Abdullah";
                     
-                   },6000);
+    },6000);
 
-                   setTimeout(() => {
+    setTimeout(() => {
                 
                     image.src = "rokon final.png";
                     paraText.innerText = "I really like your commpany products. for the first time I thought you do not have much better product in your company but now I realize no that was incorrect Thanks for your better services.";
                     nameText.innerText = "croner de";
                         
-                         },8000);
+     },8000);
 
+     
 
 
